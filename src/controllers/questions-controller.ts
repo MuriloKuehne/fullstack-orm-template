e8a50dd940f8b3038c9cf85ac5a4230f1f319335
Murilo <murkuehne@gmail.com>
2025-06-23 21:00:25 -0300

    Instalar o PostgresSQL

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,12 +1,16 @@
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from "express"
 
 class QuestionsController {
   async index(request: Request, response: Response) {
     return response.json()
   }
 
-  async create(request: Request, response: Response) {
-    return response.status(201).json()
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.status(201).json()
+    } catch (error) {
+      next(error)
+    }
   }
 
   async update(request: Request, response: Response) {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from "express"
-
+import "express-async-errors"
 import { routes } from "./routes"
 
 const PORT = 3333
